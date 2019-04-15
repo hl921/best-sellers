c067a755a3b14a16ec44598bf6622206aaec0708
Jenny Lee <haelee@umich.edu>
2019-04-15 16:26:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config = {
   NYT_KEY : "gDASL4gWnW5cMAa5JluFzl0pjyDbeB9D",// NYTimes Key,
-  GOOGLE_BOOKS_KEY : "AIzaSyBtZSP984KVxLaFAee-UndiZGsKrRyibaU" // Google Books Key
+  GOOGLE_BOOKS_KEY : "AIzaSyDvTB7DVlmJekel7lvph8qku8E3RcLDOSI" // Google Books Key
 }
